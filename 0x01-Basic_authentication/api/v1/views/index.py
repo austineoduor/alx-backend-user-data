e7b01be91689cb5d638a2265b5735945dc043037
austieoduor <austineoduor57@gmail.com>
2023-02-18 14:25:04 +0000

    file

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def not_authorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raise a 401 error by using abort
+    """
+    return abort(401)
