7935b07c60dd35b85fa64a565cad21677baf76d3
austineoduor <austineoduor57@gmail.com>
2023-02-22 09:32:20 +0000

    file

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,5 +63,4 @@ def page_forbidden(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    
     app.run(host=host, port=port)
