7125a1706704773409bb5d76b565a80f7a0b79ca
austineoduor <austineoduor57@gmail.com>
2023-02-22 20:54:00 +0000

    file user session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 '''a class SessionAuth that inherits from Auth'''
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     '''a class SessionAuth that inherits from Auth
     '''
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creates a Session ID for a user_id
+        '''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''returns a User ID based on a Session ID
+        '''
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        if session_id in self.user_id_by_session_id:
+            user_id = self.user_id_by_session_id.get(session_id)
+            return user_id
