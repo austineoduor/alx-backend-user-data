0aae43859b4f8b9371a5b80e345e1cdca3fba4b0
austineoduor <austineoduor57@gmail.com>
2023-03-03 19:11:49 +0000

    file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def playload():
-    return jsonify({'meassage': 'Bienvenue'})
+    return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
