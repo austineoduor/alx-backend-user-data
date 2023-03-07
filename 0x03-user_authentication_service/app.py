6e9fa7b3a6cb9d7cfaa2b64dea2a80d327b35a06
austineoduor <austineoduor57@gmail.com>
2023-03-07 13:09:29 +0000

    file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -92,5 +92,17 @@ def update_password():
         abort(403)
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """ user logout by destroying the session_id """
+    cookie = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(cookie)
+    if user is None:
+        abort(403)
+    else:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
